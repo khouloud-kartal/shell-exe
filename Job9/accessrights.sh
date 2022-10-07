d561b9e2858a7b06f71ecb0a57b62a16eba953ab
khouloud <khouloud@debian-BULLSEYE-live-builder-AMD64>
2022-10-07 12:53:09 +0200

    modifications

--- Job9/accessrights.sh
+++ Job9/accessrights.sh
@@ -4,16 +4,15 @@ my_csv=/home/khouloud/Shell_Userlist.csv
 
 while IFS="," read Id Prenom Nom Mdp Role;
 do 
-	#if [ "$Id" -eq "$Id" ] 2>/dev/null;
-	#then
+	if [ "$Id" -eq "$Id" ] 2>/dev/null;
+	then
 	    sudo useradd -u $Id -p $Mdp $Prenom
 	    	if [ ${Role:0:5} = "Admin" ];
 	    	then
 	    sudo usermod -aG sudo $Prenom
 	    	fi
-	#fi
+	fi
     
 
 done <$my_csv
  
-# La septième ligne sert à éviter de prendre en compte la première ligne du fichier csv
